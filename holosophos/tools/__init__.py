576ace107f4ddd601a44c2deaf3ed624512d0c49
Ilya Gusev <phoenixilya@gmail.com>
2025-01-10 17:20:58 +0000

    fixes

--- holosophos/tools/__init__.py
+++ holosophos/tools/__init__.py
@@ -5,7 +5,6 @@ from smolagents.tools import tool, Tool  # type: ignore
 from holosophos.tools.arxiv_search import arxiv_search
 from holosophos.tools.arxiv_download import arxiv_download
 from holosophos.tools.bash import bash
-from holosophos.tools.fetch import fetch
 
 
 def convert_tool_to_smolagents(function: Callable[..., Any]) -> Tool:
@@ -17,5 +16,4 @@ __all__ = [
     "arxiv_download",
     "convert_tool_to_smolagents",
     "bash",
-    "fetch",
 ]
